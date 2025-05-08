b64312dbf928d7fb1fc2c17c3ff4f24372fff29f
Ky2070 <2051052070Ky@ou.edu.vn>
2025-05-08 12:35:59 +0700

    feat: order, status-order

---
+++ QuanlyquanNet/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuanlyquanNet.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
