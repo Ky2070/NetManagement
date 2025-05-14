0b6652d30d5de1318412800816ad463a6ae11e7c
duyle27 <lehoangnhatduy1234567@gmail.com>
2025-05-14 22:11:16 +0700

    helo

--- QuanlyquanNet/Data/PhanThuongDaNhan.cs
+++ QuanlyquanNet/Data/PhanThuongDaNhan.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿namespace QuanlyquanNet.Data;
 
-using QuanlyquanNet.Data;
-
-public class PhanThuongDaNhan
+public partial class PhanThuongDaNhan
 {
     public int MaPhanThuong { get; set; }
-
     public int MaKhachHang { get; set; }
-
     public int MaPhuThuong { get; set; }
-
-    public DateTime NgayNhan { get; set; }
+    public DateTime? NgayNhan { get; set; }
 
     public virtual KhachHang MaKhachHangNavigation { get; set; } = null!;
     public virtual PhuThuong MaPhuThuongNavigation { get; set; } = null!;
