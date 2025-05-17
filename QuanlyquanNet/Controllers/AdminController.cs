95b4c65b3cffd4af11f5f2986609ed990e984a6d
Ky2070 <2051052070Ky@ou.edu.vn>
2025-05-17 17:23:28 +0700

    feat: ui for login, register

---
+++ QuanlyquanNet/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuanlyquanNet.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Admin()
+        {
+            return View();
+        }
+    }
+}
