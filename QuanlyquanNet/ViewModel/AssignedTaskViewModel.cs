9f31792a23530e5038d1514c7efd142994c8c4af
Ky2070 <2051052070Ky@ou.edu.vn>
2025-05-19 18:01:15 +0700

    feat: task random for customer

---
+++ QuanlyquanNet/ViewModel/AssignedTaskViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace QuanlyquanNet.ViewModel
+{
+    public class AssignedTaskViewModel
+    {
+        public string KhachHang { get; set; }
+        public string TenNhiemVu { get; set; }
+        public string MoTa { get; set; }
+        public string Game { get; set; }
+        public int DiemThuong { get; set; }
+        public DateTime? NgayThamGia { get; set; }
+        public string TrangThai { get; set; }
+    }
+
+}
