baf7f40b749754cb1e55d67404d4451e8f980f25
Ky2070 <2051052070Ky@ou.edu.vn>
2025-05-22 19:51:31 +0700

    fix: layout and feat: add layoutEmployee

---
+++ QuanlyquanNet/Controllers/ManagerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuanlyquanNet.Controllers
+{
+    public class ManagerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
